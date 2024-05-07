b9074a71c69f22221ae1eda9248af555e7ba267a
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2024-05-07 09:55:32 -0400

    Update app.py
    v11

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import io
 import requests
 from datetime import datetime, timedelta
 
-url='https://raw.githubusercontent.com/DuaneIndustries/DCOschedule/main/DCO_2024_Calendar_v10.csv'
+url='https://raw.githubusercontent.com/DuaneIndustries/DCOschedule/main/DCO_2024_Calendar_v11.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
