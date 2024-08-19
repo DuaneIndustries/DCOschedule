3be35f5d56584fa52dbd867f8e692cdd19777737
J Duane <69173891+DuaneIndustries@users.noreply.github.com>
2024-08-19 08:01:16 -0400

    Update app.py
    v16

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import io
 import requests
 from datetime import datetime, timedelta
 
-url='https://raw.githubusercontent.com/DuaneIndustries/DCOschedule/main/DCO_2024_Calendar_v15.csv'
+url='https://raw.githubusercontent.com/DuaneIndustries/DCOschedule/main/DCO_2024_Calendar_v16.csv'
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
@@ -57,7 +57,7 @@ app.layout = html.Div([
         dcc.DatePickerRange(
             id='date-picker-range',
             start_date="2024-08-05",
-            end_date="2024-09-29",
+            end_date="2024-10-31",
             style={'display': 'inline-block', 'float': 'right'}
         ),
         dcc.Dropdown([x for x in sorted(dff['Crew'].unique())],
